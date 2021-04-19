2f68bc203047443b28969a963049d4f143689fd7
Philipp Zerelles <pz@matrix-computer.com>
2021-04-19 13:37:31 +0200

    Added alternate routes to svelte-kit-demo

--- examples/svelte-kit-demo/svelte.config.cjs
+++ examples/svelte-kit-demo/svelte.config.cjs
@@ -4,6 +4,16 @@ const options = JSON.stringify(process.env.OPTIONS || '{}');
 module.exports = {
 	kit: {
 		adapter: adapter(options),
+		alternateRoutes: (segments) => {
+			const locales = ['de', 'en'];
+			return [
+				segments,
+				...locales.map((locale) => [
+					[{ content: locale, dynamic: false, spread: false }],
+					...segments
+				])
+			];
+		},
 		target: '#svelte'
 	}
 };
